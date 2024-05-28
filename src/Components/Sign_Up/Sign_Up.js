c954dcd005fcb18d28013367790ac8650df2b7b6
fathima722 <fathimanagia.19@gmail.com>
2024-05-28 17:52:30 -0400

    Add logout and username button

--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -1,19 +1,69 @@
-import React from "react"; 
-import './Sign_Up.css';
+import React, { useState } from 'react';
+import './Sign_Up.css'
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 
-export default function SignUp(){
+const SignUp = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState('');
+
+    const navigate = useNavigate();
+
+    const register = async (e) => {
+        e.preventDefault();
+
+        // API Call
+        const response = await fetch(`${API_URL}/api/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+                phone: phone,
+
+            }),
+        });
+
+        const json = await response.json();
+
+        if (json.authtoken) {
+            sessionStorage.setItem("auth-token", json.authtoken);
+            sessionStorage.setItem("name", name);
+            // phone and email
+            sessionStorage.setItem("phone", phone);
+            sessionStorage.setItem("email", email);
+            // Redirect to home page
+            navigate("/");   //on directing to home page you need to give logic to change login and signup buttons with name of the user and logout button where you have implemented Navbar functionality
+            window.location.reload();
+        } else {
+            if (json.errors) {
+                for (const error of json.errors) {
+                    setShowerr(error.msg);
+                }
+            } else {
+                setShowerr(json.error);
+            }
+        }
+    };
+
     return (
-        <div className="container" style={{marginTop: "5%"}}>
+        <div className="container" style={{marginTop:'5%'}}>
         <div className="signup-grid">
-            <div className="signup-text">
+        <div className="signup-text">
                 <h1>Sign Up</h1>
             </div>
             <div className="signup-text1" style={{textAlign: "left",paddingLeft: "200px"}}>
                 Already a member? <span><a href="../Login/Login.html" style={{color: "#2190FF"}}> Login</a></span>
             </div>
-            <div className="signup-form">
-                <form>
-                    <div className="form-group">
+        <div className="signup-form">
+         <form method="POST" onSubmit={register}>
+         <div className="form-group">
                         <label for="role">Role</label>
                         <select id="role" name="role" className="form-control" style={{paddingInline: "2px"}}>
                             <option value="" disabled selected>Select Role</option>
@@ -21,31 +71,36 @@ export default function SignUp(){
                             <option value="patient">Patient</option>
                         </select>
                     </div>
-                    <div className="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" id="name" required className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
-                    </div>
-                    <div className="form-group">
-                        <label for="phone">Phone</label>
-                        <input type="tel" name="phone" id="phone" required className="form-control" placeholder="Enter your phone number" pattern="[0-9]{10}" aria-describedby="helpId" />
-                        <br/>
-                        <small className="form-text text-muted">Phone number must be 10 digits.</small>
-                    </div>
-                    <div className="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" required className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
-                    </div>
-                    <div className="form-group">
-                        <label for="password">Password</label>
-                        <input name="password" id="password" required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
-                    </div>
-                    <div className="btn-group">
+           <div className="form-group">
+                <label htmlFor="email">Email</label>
+                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+                 {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+                        </div>
+
+                        <div className="form-group">
+           <label htmlFor="name">Name</label>
+           <input value={name} type="text" onChange={(e) => setName(e.target.value)} name="name" id="name" className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
+       </div>
+       <div className="form-group">
+           <label htmlFor="phone">Phone</label>
+           <input value={phone} onChange={(e) => setPhone(e.target.value)} type="tel" name="phone" id="phone" className="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
+       </div>
+       <div className="form-group">
+           <label htmlFor="password">Password</label>
+           <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
+
+       </div>
+       <div className="btn-group">
                         <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
                         <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
                     </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    )
+         </form>
+         </div>
+         </div>
+         </div>
+         
+ //Sign up role is not stored in database. You can apply logic for this according to your react code.
+    );
 }
+
+export default SignUp;
