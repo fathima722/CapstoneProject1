a3bb056e6fd9e9650f6f77ed6b5e19c363b84591
fathima722 <fathimanagia.19@gmail.com>
2024-05-28 14:52:46 -0400

    database_setup

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:OTQ1Ni1mYXRoaW1h@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
